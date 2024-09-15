eefc04d7601593e5bab8273fce475772698c91b9
Rudalph <gonsalvesrudalph@gmail.com>
2024-09-15 16:13:40 +0530

    Layout.tsx updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,16 +5,16 @@ import { Nunito } from "next/font/google"
 
 const nunito = Nunito({ subsets: ["latin"], weight:['200','300','400','500','600','700','800','900']});
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
