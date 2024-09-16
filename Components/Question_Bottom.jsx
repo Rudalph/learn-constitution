b4f6ebffa0aae469e1e8d87dcb6897a8f2780ec1
Rudalph <gonsalvesrudalph@gmail.com>
2024-09-16 16:51:10 +0530

    Question Answring Section updated

---
+++ Components/Question_Bottom.jsx
@@ -0,0 +1,39 @@
+import React,{useState} from 'react'
+import Image from 'next/image'
+import { MdBugReport } from "react-icons/md";
+
+const Question_Bottom = ({setCorrect}) => {
+
+    const [changes, setChanges] = useState(true)
+    const check = (e) =>{
+        setCorrect(true);
+        setChanges(false);
+    }
+
+    return (
+        <div>
+
+            <div className='flex justify-between lg:w-[900px] flex-wrap gap-5 pt-12 pb-12 h-36'>
+                {changes ? <button className="btn w-40 rounded-2xl bg-[white] text-[#9D9D9D] border border-[#9D9D9D] font-extrabold text-lg shadow-[0_5px_0_#9D9D9D] hover:bg-[#E5E5E5]">SKIP</button> :
+                  <div className='flex justify-center align-middle items-center gap-5'>
+                    <Image src="/tick.png" height={60} width={60} className='p-2 bg-white rounded-full animate-bounce'/>
+                    <div>
+                        <p className='text-xl font-extrabold text-[#58A700]'>Great, you are correct!</p>
+                        <br />
+                       <button className='flex justify-start align-middle items-center gap-2'>
+                          <MdBugReport size={25} className='text-[#58A700]'/>
+                          <p className='flex text-base font-extrabold text-[#61B800]'> Report</p>
+                       </button>
+                    </div>
+                    
+                  </div>
+                }
+                
+                <button className="btn w-40 rounded-2xl bg-[#58CC02] text-white font-extrabold border-[#58CC02] text-lg shadow-[0_5px_0_#3FA001] hover:bg-[#61E002]" onClick={check}>{changes ? 'CHECK' : 'CONTINUE'}</button>
+            </div>
+
+        </div>
+    )
+}
+
+export default Question_Bottom
--- app/Audios/page.jsx
+++ app/Audios/page.jsx
@@ -1,11 +1,10 @@
 "use client"
-import React,{useState} from 'react'
+import React from 'react'
 import Image from 'next/image'
 import Question_Bottom from '@/Components/Question_Bottom'
 
 
 const page = () => {
-    const [correct, setCorrect] = useState(false)
     return (
         <div className='flex flex-col justify-center items-center '>
             <div className='flex justify-center align-middle items-center mt-5 lg:mt-9'>
@@ -23,7 +22,12 @@ const page = () => {
                 <p className='text-center'>The question will be MCQ-based and will be asked on the Constitution of India, Part 4 (Union) and Part 5 (States)?</p>
             </div>
 
-            <div className='flex justify-around gap-6 align-middle items-center mt-8 lg:mt-24 flex-wrap p-1'>
+            <audio controls className='mt-10'>
+                <source src="https://actions.google.com/sounds/v1/alarms/alarm_clock.ogg" type="audio/ogg"/>
+                Your browser does not support the audio element.
+            </audio>
+
+            <div className='flex justify-around gap-6 align-middle items-center mt-8 lg:mt-10 flex-wrap p-1'>
                 <div className="card border-2 border-[#E5E5E5] lg:w-96" >
                     <div className="card-body p-2">
                         <h2 className="card-title text-lg text-[#4B4B4B] font-extrabold"></h2>
@@ -75,10 +79,8 @@ const page = () => {
                 </div>
             </div>
 
-            
-
-            <div className={`hidden lg:fixed bottom-0 z-50 border-t-2 border-[#E5E5E5] w-full lg:flex justify-center align-middle items-center ${correct ? 'bg-[#D7FFB8]' : ''}`}>
-              <Question_Bottom setCorrect={setCorrect}/>
+            <div className='hidden lg:fixed bottom-0 z-50 border-t-2 border-[#E5E5E5] w-full lg:flex justify-center align-middle items-center'>
+              <Question_Bottom />
             </div>
         </div>
 
