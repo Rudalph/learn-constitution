681827e0e2c87c7ebd62d95ab287a4b5423b2875
JonathanDabre <jonathandabre01@gmail.com>
2024-09-16 17:54:28 +0530

    Structure changed, created Each section component

--- Components/MiddleSection.tsx
+++ Components/MiddleSection.tsx
@@ -1,24 +1,11 @@
 import React from 'react'
-import UnitIndicator from './UnitIndicator'
-import Unit from './Unit'
+import EachSection from './EachSection'
+
 
 const MiddleSection = () => {
   return (
-    <div className='middle-section relative'>
-        <div className="UnitIndicator mt-10 px-5  sticky top-10 w-[100%] z-10">
-            <UnitIndicator section={1} unit={1} title={'Pair letters and sounds'}/>
-        </div>
-        <div className="flex flex-col">
-            <div id='unit1' className='mt-[60px]'>
-                <Unit levels={5} unit={1} nextTopic={'Pair letters and sounds '} activeCount={2}/>
-            </div>
-            <div id='unit2' className=''>
-                <Unit levels={5} unit={2} nextTopic={'Pair letters and sounds '} activeCount={0}/>
-            </div>
-            <div id='unit3' className=''>
-                <Unit levels={5} unit={3} nextTopic={'Pair letters and sounds '} activeCount={0}/>
-            </div>
-        </div>
+    <div className='middle-section'> 
+        <EachSection/>
     </div>
   )
 }
---
+++ app/Data/curriculum.js
@@ -0,0 +1,241 @@
+const curriculumProgressData = {
+    "syllabus": [
+      {
+        "Section 1": "Title of Section 1",
+        "completed": false,
+        "units": [
+          {
+            "unit": "Unit 1",
+            "completed": 3,
+            "chapters": [
+              {
+                "chapter": "Chapter 1",
+                "link": "/section1/unit1/chapter1/"
+              },
+              {
+                "chapter": "Chapter 2",
+                "link": "/section1/unit1/chapter2/"
+              },
+              {
+                "chapter": "Chapter 3",
+                "link": "/section1/unit1/chapter3/"
+              },
+              {
+                "chapter": "Chapter 4",
+                "link": "/section1/unit1/chapter4/"
+              },
+              {
+                "chapter": "Chapter 5",
+                "link": "/section1/unit1/chapter5/"
+              }
+            ]
+          },
+          {
+            "unit": "Unit 2",
+            "completed": 0,
+            "chapters": [
+              {
+                "chapter": "Chapter 1",
+                "link": "/section1/unit2/chapter1/"
+              },
+              {
+                "chapter": "Chapter 2",
+                "link": "/section1/unit2/chapter2/"
+              },
+              {
+                "chapter": "Chapter 3",
+                "link": "/section1/unit2/chapter3/"
+              },
+              {
+                "chapter": "Chapter 4",
+                "link": "/section1/unit2/chapter4/"
+              },
+              {
+                "chapter": "Chapter 5",
+                "link": "/section1/unit2/chapter5/"
+              },
+              {
+                "chapter": "Chapter 6",
+                "link": "/section1/unit2/chapter6/"
+              }
+            ]
+          },
+          {
+            "unit": "Unit 3",
+            "completed": 0,
+            "chapters": [
+              {
+                "chapter": "Chapter 1",
+                "link": "/section1/unit3/chapter1/"
+              },
+              {
+                "chapter": "Chapter 2",
+                "link": "/section1/unit3/chapter2/"
+              },
+              {
+                "chapter": "Chapter 3",
+                "link": "/section1/unit3/chapter3/"
+              },
+              {
+                "chapter": "Chapter 4",
+                "link": "/section1/unit3/chapter4/"
+              },
+              {
+                "chapter": "Chapter 5",
+                "link": "/section1/unit3/chapter5/"
+              },
+              {
+                "chapter": "Chapter 6",
+                "link": "/section1/unit3/chapter6/"
+              }
+            ]
+          },
+          {
+            "unit": "Unit 4",
+            "completed": 0,
+            "chapters": [
+              {
+                "chapter": "Chapter 1",
+                "link": "/section1/unit4/chapter1/"
+              },
+              {
+                "chapter": "Chapter 2",
+                "link": "/section1/unit4/chapter2/"
+              },
+              {
+                "chapter": "Chapter 3",
+                "link": "/section1/unit4/chapter3/"
+              },
+              {
+                "chapter": "Chapter 4",
+                "link": "/section1/unit4/chapter4/"
+              },
+              {
+                "chapter": "Chapter 5",
+                "link": "/section1/unit4/chapter5/"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "Section 2": "Physics",
+        "completed": false,
+        "units": [
+          {
+            "unit": "Unit 1",
+            "completed": 0,
+            "chapters": [
+              {
+                "chapter": "Chapter 1",
+                "link": "/section2/unit1/chapter1/"
+              },
+              {
+                "chapter": "Chapter 2",
+                "link": "/section2/unit1/chapter2/"
+              },
+              {
+                "chapter": "Chapter 3",
+                "link": "/section2/unit1/chapter3/"
+              },
+              {
+                "chapter": "Chapter 4",
+                "link": "/section2/unit1/chapter4/"
+              },
+              {
+                "chapter": "Chapter 5",
+                "link": "/section2/unit1/chapter5/"
+              }
+            ]
+          },
+          {
+            "unit": "Unit 2",
+            "completed": 0,
+            "chapters": [
+              {
+                "chapter": "Chapter 1",
+                "link": "/section2/unit2/chapter1/"
+              },
+              {
+                "chapter": "Chapter 2",
+                "link": "/section2/unit2/chapter2/"
+              },
+              {
+                "chapter": "Chapter 3",
+                "link": "/section2/unit2/chapter3/"
+              },
+              {
+                "chapter": "Chapter 4",
+                "link": "/section2/unit2/chapter4/"
+              },
+              {
+                "chapter": "Chapter 5",
+                "link": "/section2/unit2/chapter5/"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "Section 3": "Chemistry",
+        "completed": false,
+        "units": [
+          {
+            "unit": "Unit 1",
+            "completed": 0,
+            "chapters": [
+              {
+                "chapter": "Chapter 1",
+                "link": "/section3/unit1/chapter1/"
+              },
+              {
+                "chapter": "Chapter 2",
+                "link": "/section3/unit1/chapter2/"
+              },
+              {
+                "chapter": "Chapter 3",
+                "link": "/section3/unit1/chapter3/"
+              },
+              {
+                "chapter": "Chapter 4",
+                "link": "/section3/unit1/chapter4/"
+              },
+              {
+                "chapter": "Chapter 5",
+                "link": "/section3/unit1/chapter5/"
+              }
+            ]
+          },
+          {
+            "unit": "Unit 2",
+            "completed": 0,
+            "chapters": [
+              {
+                "chapter": "Chapter 1",
+                "link": "/section3/unit2/chapter1/"
+              },
+              {
+                "chapter": "Chapter 2",
+                "link": "/section3/unit2/chapter2/"
+              },
+              {
+                "chapter": "Chapter 3",
+                "link": "/section3/unit2/chapter3/"
+              },
+              {
+                "chapter": "Chapter 4",
+                "link": "/section3/unit2/chapter4/"
+              },
+              {
+                "chapter": "Chapter 5",
+                "link": "/section3/unit2/chapter5/"
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  };
+  
+  export default curriculumProgressData;
+  
