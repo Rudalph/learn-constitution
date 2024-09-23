82287ad61cf535c6c54ac80afca654cd1b119ec3
JonathanDabre <jonathandabre01@gmail.com>
2024-09-23 23:30:36 +0530

    ui Fixes images

--- app/Images/page.jsx
+++ app/Images/page.jsx
@@ -10,19 +10,19 @@ const Page = () => {
     return (
         <div className='flex flex-col justify-center items-center '>
             <Header_Of_Questions />
-            <div className='flex justify-between align-middle items-center flex-wrap'>
-                <div className='lg:h-96 lg:w-[500px] flex justify-center align-middle items-center mt-10 p-5 '>
+            <div className='flex justify-between align-middle flex-wrap'>
+                <div className='lg:h-96 lg:w-[40%] flex justify-center align-middle  p-5 '>
                     <img src="https://blog.ipleaders.in/wp-content/uploads/2020/11/Constitution-of-India-1024x576.png" alt="#" className=' object-cover rounded-lg' />
                 </div>
 
 
 
-                <div className='lg:p-10 items-end'>
-                    <div className='lg:w-[700px] flex justify-center align-middle items-center lg:ml-10  mt-10 text-[#3C3C3C] font-extrabold text-lg text-wrap lg:text-2xl'>
-                        <p className='text-center'>The question will be MCQ-based and will be asked on the Constitution of India, Part 4 (Union) and Part 5 (States)?</p>
+                <div className='lg:p-10 w-[60%] items-end'>
+                    <div className=' flex justify-center align-middle items-center  mt-10 text-[#3C3C3C] font-extrabold text-lg text-wrap lg:text-2xl'>
+                        <p className=''>The question will be MCQ-based and will be asked on the Constitution of India, Part 4 (Union) and Part 5 (States)?</p>
                     </div>
 
-                    <div className='flex justify-around gap-6 align-middle items-center mt-8 lg:mt-24 flex-wrap p-1'>
+                    <div className='flex justify-around gap-6 align-middle items-center mt-4 p-1'>
                         <div className="card border-2 border-[#E5E5E5] lg:w-96" >
                             <div className="card-body p-2">
                                 <h2 className="card-title text-lg text-[#4B4B4B] font-extrabold"></h2>
@@ -48,7 +48,7 @@ const Page = () => {
                         </div>
                     </div>
 
-                    <div className='flex justify-around gap-6 align-middle items-center mt-7 flex-wrap p-1'>
+                    <div className='flex justify-around gap-6 align-middle items-center mt-2  p-1'>
                         <div className="card border-2 border-[#E5E5E5] lg:w-96">
                             <div className="card-body p-2">
                                 <h2 className="card-title text-lg text-[#4B4B4B] font-extrabold"></h2>
